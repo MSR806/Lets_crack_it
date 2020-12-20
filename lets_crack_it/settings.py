62e0e689f4d56e96165e783f9954d78da783d016
MSR806 <msr0806@users.noreply.github.com>
2020-12-20 22:20:02 +0530

    #CSRF_COOKIE_SECURE = True

--- lets_crack_it/settings.py
+++ lets_crack_it/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'agd&pkh^(@yz3z%68qi))n!%@8a99r$th*1c9sbxo^uqvv(695'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 
 ALLOWED_HOSTS = ['*']
